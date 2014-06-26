99118bf8bd45238cc207d600fa0ba6677ab246b9
Bezbo <without.a.beard@gmail.com>
2014-06-26 15:37:45 +0400

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "home page" do
+
+    it "should have content 'Tutorial App' " do
+      visit '/static_pages/home'
+      expect(page).to have_content("Tutorial App")
+    end
+
+    it "should have right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+  end
+
+  describe "help page" do
+
+    it "should have content 'help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content("Help")
+    end
+
+    it "should have right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
+  end
+
+  describe "about us page" do
+
+    it "should have content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content("About Us")
+    end
+
+    it "should have right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About Us")
+    end
+  end
+
+end
