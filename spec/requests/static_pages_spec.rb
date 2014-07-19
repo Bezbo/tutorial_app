cb8c6d906dcca1f841d2ccb236133884d619ac76
Bezbo <without.a.beard@gmail.com>
2014-07-20 00:55:22 +0400

    done exercises 5.6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,33 +4,61 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector("h1", text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "home page" do
     before{ visit root_path }
 
-    it { should have_content("Tutorial App") }
-    it { should have_title("Tutorial App") }
+    let(:heading)    { "Tutorial App" }
+    let(:page_title) { "" }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title("| Home") }
   end
 
   describe "help page" do
     before{ visit help_path }
 
-    it { should have_content("Help") }
-    it { should have_title("Help") }
+    let(:heading)    { "Help" }
+    let(:page_title) { "Help" }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "about us page" do
     before{ visit about_path }
 
-    it { should have_content("About Us") }
-    it { should have_title("About Us") }
+    let(:heading)    { "About Us" }
+    let(:page_title) { "About Us" }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "contact page" do
     before{ visit contact_path }
 
-    it { should have_content("Contact") }
-    it { should have_title("Contact") }
+    let(:heading)    { "Contact" }
+    let(:page_title) { "Contact" }
+
+    it_should_behave_like "all static pages"
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+
+    click_link "About"
+    expect(page).to have_title(full_title("About Us"))
+    click_link "Help"
+    expect(page).to have_title(full_title("Help"))
+    click_link "Contact"
+    expect(page).to have_title(full_title("Contact"))
+    click_link "Home"
+    click_link "Sign up!"
+    expect(page).to have_title(full_title("Sign up"))
+    click_link "tutorial app"
+    expect(page).to have_selector("h1", text: "Tutorial App")
+  end
 end
