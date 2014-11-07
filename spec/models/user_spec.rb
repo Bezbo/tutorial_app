50f7e90faceec772760a12803f81996b8f1c7ea2
Bezbo <without.a.beard@gmail.com>
2014-11-07 23:44:52 +0300

    finish user edit, update, index, destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,8 +13,19 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "remember token" do
     before { @user.save }
@@ -97,7 +108,7 @@ describe User do
 
     describe "with invalid password" do
       let (:user_for_invalid_password) { found_user.authenticate("invalid") }
-      
+
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
